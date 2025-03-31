87dde4c75dfa91f189dd7c251f9a43f545dc00b6
gusthls <gustavohls_work@hotmail.com>
2025-03-31 14:51:37 -0300

    Princípio I adicionado, criação de interfaces, facades

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ from cliente import Cliente
 from item import Item
 from pedido.pedido_retirada import PedidoRetirada
 from pedido.pedido_delivery import PedidoDelivery
-from pagamento.pagamento_cartao import PagamentoCartao
-from pagamento.pagamento_pix import PagamentoPIX
 from pagamento.pagamento_factory import PagamentoFactory
+from notificacao.notificacao_facade import NotificacaoFacade
 
-cliente = Cliente("João", "Rua A, 123")
+cliente = Cliente("Gustavo", "Rua Alegre, 123")
 item_1 = Item("Camiseta", 29.99)
 item_2 = Item("Calça Jeans", 49.99)
-print(f"Item: {item_1.nome}, Preço: {item_1.preco}")
-print(f"Item: {item_2.nome}, Preço: {item_2.preco}")
-print(f"Nome: {cliente.nome}, Endereço: {cliente.endereco}")
 
 itens = [item_1, item_2]
 pedido_retirada = PedidoRetirada(cliente, itens)
@@ -23,3 +19,7 @@ valor_pedido = pedido_delivery.calcular_total()
 tipo_pagamento = "pix"
 pagamento = PagamentoFactory().criar_pagamento(tipo_pagamento)
 pagamento.processar(valor_pedido)
+
+MENSAGEM = "Seu pedido saiu para entrega!"
+notificacao = NotificacaoFacade()
+notificacao.enviar_notificacao(cliente, MENSAGEM)
